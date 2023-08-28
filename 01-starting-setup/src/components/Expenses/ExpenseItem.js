3b6c360c562b4db5b102810f7722fbc33a0819ef
Dodam <dodam0724@gmail.com>
2023-08-29 00:01:05 +0900

    Update ExpenseItem.js

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,7 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
   // function clickHandler() {}
 
-  const clickHandler = () => { // (이벤트 핸들러 함수) + Handler: 이벤트 발생 시, 리액트가 호출하는 함수
+  const clickHandler = () => { // (이벤트 핸들러 함수 끝에 +Handler) : 이벤트 발생 시, 리액트가 호출하는 함수
     console.log('Clicked!!!');
   };
 
