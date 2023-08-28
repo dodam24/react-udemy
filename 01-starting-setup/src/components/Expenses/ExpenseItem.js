fab30c102c777d83ad69c8b6b826d1f9ca61a76d
dodam24 <dodam0724@gmail.com>
2023-08-28 23:59:36 +0900

    리액트 State 및 이벤트 다루기

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,7 +4,13 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+
+  const clickHandler = () => { // (이벤트 핸들러 함수) + Handler: 이벤트 발생 시, 리액트가 호출하는 함수
+    console.log('Clicked!!!');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />  
@@ -12,6 +18,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
