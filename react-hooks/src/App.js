ea4f384fe8887e289ef8c5a0d22837183cd37e50
dodam24 <dodam0724@gmail.com>
2023-09-12 23:56:04 +0900

    React Hooks - useState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import Timer from './component/Timer';
+import { useState } from 'react';
+
+const heavyWork = () => {
+  console.log("엄청 무거운 작업!!!");
+  return ['홍길동', '김민수'];
+}
 
 function App() {
-  const [showTimer, setShowTimer] = useState(false);
+  // 맨 처음 렌더링 될 때만 함수 호출
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      console.log('이전 state: ', prevState);
+      return [input, ...prevState];
+    });
+  };
 
   return (
     <div>
-      {showTimer && <Timer />}
-      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+      <input type="text" value={input} onChange={handleInputChange}/>
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
