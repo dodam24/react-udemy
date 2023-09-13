febb3a8c6e3a35ac5febe080e3aa3ed784ccc227
dodam24 <dodam0724@gmail.com>
2023-09-13 13:42:39 +0900

    React Hooks - useContext, Context API

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,23 +1,18 @@
-import React, { useEffect, useRef } from 'react';
+import { useState } from 'react';
+import './App.css';
+import Page from './component/Page';
+import { ThemeContext } from './context/ThemeContext';
+import { UserContext } from './context/UserContext';
 
 const App = () => {
-  const inputRef = useRef();
-
-  useEffect(() => {
-    // console.log(inputRef);
-    inputRef.current.focus();
-  }, []);
-
-  const login = () => {
-    alert(`환영합니다 ${inputRef.current.value}!`)
-    inputRef.current.focus();
-  };
+  const [isDark, setIsDark] = useState(false);
 
   return (
-    <div>
-      <input ref={inputRef} type="text" placeholder="username" />
-      <button onClick={login}>로그인</button>
-    </div>
+    <UserContext.Provider value={'사용자'}>
+    <ThemeContext.Provider value={{ isDark, setIsDark }}>
+      <Page />
+    </ThemeContext.Provider>
+    </UserContext.Provider>
   );
 }
 
